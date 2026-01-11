748db7fc6a75b51cbd612aa55e8404e6d85962b0
shamil-khan <shamil.ahmed@gmail.com>
2026-01-11 05:36:49 +0500

    moved processing to header

--- src/components/library/FileProcessingCategoryBar.tsx
+++ src/components/library/FileProcessingCategoryBar.tsx
@@ -1,25 +1,14 @@
-import { Button } from '@/components/ui/button';
 import { CategorySelector } from '@/components/CategorySelector';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { Download, Tag } from 'lucide-react';
+import { Tag } from 'lucide-react';
 
 interface FileProcessingCategoryBarProps {
   selectedCategoryIds: number[];
   onSelectedCategoryIdsChange: (categoryIds: number[]) => void;
-  onProcessTitles: (categoryIds?: number[]) => void;
-  disabled: boolean;
 }
 
 export const FileProcessingCategoryBar = ({
   selectedCategoryIds,
   onSelectedCategoryIdsChange,
-  onProcessTitles,
-  disabled,
 }: FileProcessingCategoryBarProps) => {
   return (
     <div className='px-3 pt-3 pb-1 flex items-center justify-between'>
@@ -35,23 +24,6 @@ export const FileProcessingCategoryBar = ({
           onCategoryChange={onSelectedCategoryIdsChange}
         />
       </div>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              size='icon'
-              variant='ghost'
-              onClick={() => onProcessTitles(selectedCategoryIds)}
-              disabled={disabled}
-              className='h-8 w-8'>
-              <Download className='w-4 h-4' />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Download movie info with poster</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
     </div>
   );
 };
